c517a2b423b13cfd26f4fb62872c9b0dd93f1536
GenisGenerik <164584630+GenisGenerik@users.noreply.github.com>
2026-09-15 18:49:36 +0700

    Wire lifecycle compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.tv.foundation)
     implementation(libs.androidx.tv.material)
     implementation("androidx.compose.material3:material3:1.3.1")
